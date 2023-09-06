a1ff88129f207c27c2d0d30dc646bcb394ad03f3
Sumitmaithani <sumitmaithani824@gmail.com>
2023-09-06 12:48:04 +0530

    remove commented code

--- app/api/courses/[id]/route.js
+++ app/api/courses/[id]/route.js
@@ -9,68 +9,6 @@ export async function GET(request, { params }) {
   return NextResponse.json({ course }, { status: 200 });
 }
 
-// export async function PUT(request, { params }) {
-//   const { id } = params;
-
-//   const { sectionIndex, moduleIndex } = await request.json(); // Assuming you're sending the sectionIndex and moduleIndex from the request body
-
-//   await connectMongoDB();
-
-//   try {
-//     const updatedCourse = await Course.findByIdAndUpdate(
-//       id,
-//       {
-//         $set: {
-//           [`Content.${sectionIndex}.modules.${moduleIndex}.status`]: "Pass"
-//         }
-//       },
-//       { new: true }
-//     );
-
-//     return NextResponse.json(
-//       { message: "Course updated", course: updatedCourse },
-//       { status: 200 }
-//     );
-//   } catch (error) {
-//     console.error("Error updating module status:", error);
-//     return NextResponse.json(
-//       { message: "Error updating course" },
-//       { status: 500 }
-//     );
-//   }
-// }
-
-// export async function PUT(request, { params }) {
-//   const { id } = params;
-
-//   const { competencyIndex } = await request.json(); // Assuming you're sending the competencyIndex from the request body
-
-//   await connectMongoDB();
-
-//   try {
-//     const updatedCourse = await Course.findByIdAndUpdate(
-//       id,
-//       {
-//         $set: {
-//           [`Competencies.${competencyIndex}.status`]: "Pass"
-//         }
-//       },
-//       { new: true }
-//     );
-
-//     return NextResponse.json(
-//       { message: "Course updated", course: updatedCourse },
-//       { status: 200 }
-//     );
-//   } catch (error) {
-//     console.error("Error updating competency status:", error);
-//     return NextResponse.json(
-//       { message: "Error updating course" },
-//       { status: 500 }
-//     );
-//   }
-// }
-
 export async function PUT(request, { params }) {
   const { id } = params;
 
